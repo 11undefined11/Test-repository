4d395d0b9cf5296fb848063c1a0cd3cffd447637
MazdakShafieiSabet <m.shafiei@europagym.at>
2025-02-01 13:51:23 +0100

    Cleaned up code a bit, added support for different msys64 binary paths.

--- helloworld.c
+++ helloworld.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <limits.h>
 #include <float.h>
-int main()
-{
-    printf("size of int %d, limits of int; max: %d; min: %d \n", sizeof(int), INT_MIN, INT_MIN, '\n');
-    printf("size of float %d, max: %d, min: %d \n", sizeof(float), FLT_MAX, FLT_MIN);
-    printf("size of char %d; limits of char; max: %d; min: %d \n", sizeof(char), CHAR_MIN, CHAR_MAX);
-    printf("size of signed char %d \n", sizeof(signed char), '\n');
-    printf("size of unsigned char %d \n", sizeof(unsigned char), '\n');
-    printf("size of short %d \n", sizeof(short), '\n');
-    printf("size of short %d \n", sizeof(short), '\n');
-    printf("size of signed short %d \n", sizeof(signed short), '\n');
-    printf("size of signed short int %d \n", sizeof(signed short int), '\n');
-    printf("size of signed %d \n", sizeof(signed), '\n');
-    printf("size of signed int %d \n", sizeof(signed int), '\n');
-    printf("size of unsigned %d \n", sizeof(unsigned), '\n');
-    printf("size of unsigned int %d \n", sizeof(unsigned int), '\n');
-    printf("size of long %d \n", sizeof(long), '\n');
-    printf("size of long int %d \n", sizeof(long int), '\n');
-    printf("size of signed long %d \n", sizeof(signed long), '\n');
-    printf("size of signed long int %d \n", sizeof(signed long int), '\n');
-    printf("size of unsigned long %d \n", sizeof(unsigned long), '\n');
-    printf("size of unsigned long int %d \n", sizeof(unsigned long int), '\n');
-    printf("size of long long %d \n", sizeof(long long), '\n');
-    printf("size of long long int %d \n", sizeof(long long int), '\n');
-    printf("size of signed long long %d \n", sizeof(signed long long), '\n');
-    printf("size of signed long long int %d \n", sizeof(signed long long int), '\n');
-    printf("size of unsigned long long %d \n", sizeof(unsigned long long), '\n');
-    printf("size of unsigned long long int %d \n", sizeof(unsigned long long int), '\n');
-    printf("size of double %d \n", sizeof(double), '\n');
-    printf("size of long double %d \n", sizeof(long double), '\n');
+
+int main() {
+    printf("size of int: %zu, limits of int; max: %d; min: %d\n", sizeof(int), INT_MAX, INT_MIN);
+    printf("size of float: %zu, max: %e, min: %e\n", sizeof(float), FLT_MAX, FLT_MIN);
+    printf("size of char: %zu, limits of char; max: %d; min: %d\n", sizeof(char), CHAR_MAX, CHAR_MIN);
+    printf("size of signed char: %zu\n", sizeof(signed char));
+    printf("size of unsigned char: %zu\n", sizeof(unsigned char));
+    printf("size of short: %zu\n", sizeof(short));
+    printf("size of signed short: %zu\n", sizeof(signed short));
+    printf("size of signed short int: %zu\n", sizeof(signed short int));
+    printf("size of signed: %zu\n", sizeof(signed));
+    printf("size of signed int: %zu\n", sizeof(signed int));
+    printf("size of unsigned: %zu\n", sizeof(unsigned));
+    printf("size of unsigned int: %zu\n", sizeof(unsigned int));
+    printf("size of long: %zu\n", sizeof(long));
+    printf("size of long int: %zu\n", sizeof(long int));
+    printf("size of signed long: %zu\n", sizeof(signed long));
+    printf("size of signed long int: %zu\n", sizeof(signed long int));
+    printf("size of unsigned long: %zu\n", sizeof(unsigned long));
+    printf("size of unsigned long int: %zu\n", sizeof(unsigned long int));
+    printf("size of long long: %zu\n", sizeof(long long));
+    printf("size of long long int: %zu\n", sizeof(long long int));
+    printf("size of signed long long: %zu\n", sizeof(signed long long));
+    printf("size of signed long long int: %zu\n", sizeof(signed long long int));
+    printf("size of unsigned long long: %zu\n", sizeof(unsigned long long));
+    printf("size of unsigned long long int: %zu\n", sizeof(unsigned long long int));
+    printf("size of double: %zu\n", sizeof(double));
+    printf("size of long double: %zu\n", sizeof(long double));
     return 0;
 }
